104fa04c8b2a1b53731e42c42d49a6f5293bcf80
revathinair9620 <revathymadhusudhan1994@gmail.com>
2024-03-26 16:57:13 +1100

    made changes to mainamenupage

--- src/main/java/com/weberp/qa/pages/MainMenuPage.java
+++ src/main/java/com/weberp/qa/pages/MainMenuPage.java
@@ -1,7 +1,16 @@
 package com.weberp.qa.pages;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -10,6 +19,9 @@ import org.openqa.selenium.support.PageFactory;
 import com.weberp.qa.base.TestBase;
 
 public class MainMenuPage extends TestBase {
+
+	private static final String EXCEL_FILE_PATH = "C:\\Users\\Sam\\eclipse-workspace\\Weberpproject\\src"
+			+ "\\main\\java\\com\\weberp\\qa\\testdata\\webERP.xlsx";
 
 	// pagefactory OR
 
@@ -22,6 +34,13 @@ public class MainMenuPage extends TestBase {
 	@FindBy(xpath = "//b[contains(text(),'Maintenance')]")
 	WebElement maintenance;
 	
+	@FindBy(xpath = "/html/body/section/fieldset[1]/ul[1]/li")
+	private List<WebElement> menuItems;
+	
+
+	
+	//@FindBy(xpath = "//ul[@xpath='1']/li[@class='MenuItem']")
+    
 
 	// Initializing the Page Objects:
 	public MainMenuPage() {
@@ -30,11 +49,32 @@ public class MainMenuPage extends TestBase {
 
 	// Actions:
 	public List<WebElement> getMenuItemsTranscationTab() {
-        return driver.findElements(By.xpath("/html/body/section/fieldset[1]/ul")); 
+		return driver.findElements(By.xpath("/html/body/section/fieldset[1]/ul[1]/li"));
+
+	}
+
+	public List<WebElement> getMenuItemsInquiriesReportTab() {
+		return driver.findElements(By.xpath("/html/body/section/fieldset[2]/ul[1]/li"));
+	}
+
+	public List<WebElement> getMenuItemsMaintenanceTab() {
+		return driver.findElements(By.xpath("/html/body/section/fieldset[3]/ul[1]/li"));
+	}
+
+	public List<WebElement> getMainMenuTab() {
+		return driver.findElements(By.xpath("//body/nav[1]/ul[1]/li"));
+	}
+	
+	public List<String> getAllMenuItems() {
+        List<String> actualMenuItems = new ArrayList<>();
+        for (WebElement menuItem : menuItems) {
+            String menuItemText = menuItem.getText().trim();
+            actualMenuItems.add(menuItemText);
+            System.out.println("Menu Item: " + menuItemText);
+        }
+        return actualMenuItems;
     }
 	
-	public List<WebElement> getMenuItemsInquiriesReportTab() {
-        return driver.findElements(By.xpath("/html/body/section/fieldset[2]/ul")); 
-    }
+	
 
 }
--- src/test/java/com/weberp/qa/testcases/MainMenuPageTest.java
+++ src/test/java/com/weberp/qa/testcases/MainMenuPageTest.java
@@ -1,8 +1,9 @@
 package com.weberp.qa.testcases;
 
+import java.util.Arrays;
 import java.util.List;
-
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -14,6 +15,18 @@ public class MainMenuPageTest extends TestBase {
 
 	LoginPage loginPage;
 	MainMenuPage mainMenuPage;
+	
+	private List<String> expectedMenuItems = Arrays.asList(
+            "• New Sales Order or Quotation",
+            "• Enter Counter Sales",
+            "• Enter Counter Returns",
+            "• Generate/Print Picking Lists",
+            "• Outstanding Sales Orders/Quotations",
+            "• Special Order",
+            "• Recurring Order Template",
+            "• Process Recurring Orders",
+            "• Maintain Picking Lists"
+    );
 
 	public MainMenuPageTest() {
 		super(); // call the test base class constructor
@@ -31,28 +44,60 @@ public class MainMenuPageTest extends TestBase {
 	public void verifyTranscationMenuItems() {
 		List<WebElement> menuItems = mainMenuPage.getMenuItemsTranscationTab();
 		System.out.println("Size of elementList: " + menuItems.size());
-		System.out.println("Menu Items:");
 		for (WebElement menuItem : menuItems) {
-			// System.out.println("Actual Menu Item Text: \n " + menuItem.getText());
 			String actualMenuItemText = menuItem.getText().trim();
-			System.out.println("Actual Menu Item Text: \n" + actualMenuItemText);
+			System.out.println("Actual Menu Item Text:" + actualMenuItemText);
 		}
 
 	}
+
 	@Test
 	public void verifyInquiriesReportItems() {
 		List<WebElement> menuItems = mainMenuPage.getMenuItemsInquiriesReportTab();
 		System.out.println("Size of elementList: " + menuItems.size());
-		System.out.println("Menu Items:");
 		for (WebElement menuItem : menuItems) {
-			// System.out.println("Actual Menu Item Text: \n " + menuItem.getText());
 			String actualMenuItemText = menuItem.getText().trim();
-			System.out.println("Actual Menu Item Text: \n" + actualMenuItemText);
-
+			System.out.println("Actual Menu Item Text:" + actualMenuItemText);
 
 		}
 
 	}
+
+	@Test
+	public void verifyMaintenanaceItems() {
+		List<WebElement> menuItems = mainMenuPage.getMenuItemsMaintenanceTab();
+		System.out.println("Size of elementList: " + menuItems.size());
+		for (WebElement menuItem : menuItems) {
+			String actualMenuItemText = menuItem.getText().trim();
+			System.out.println("Actual Menu Item Text:" + actualMenuItemText);
+//			String expectedMenuItemText = "• Create Contract\r\n" + "• Select Contract\r\n"
+//					+ "• Sell Through Support Deals";
+//			Assert.assertEquals(actualMenuItemText, expectedMenuItemText, "Menu item text doesn't match expected.");
+
+		}
+
+	}
+
+	@Test
+	public void verifyMainmenuItems() {
+		List<WebElement> menuItems = mainMenuPage.getMainMenuTab();
+		System.out.println("Size of elementList: " + menuItems.size());
+		for (WebElement menuItem : menuItems) {
+			String actualMenuItemText = menuItem.getText().trim();
+			System.out.println("Actual Menu Item Text:" + actualMenuItemText);
+
+		}
+
+	}
+	
+	@Test
+    public void testMenuItems() {
+        List<String> actualMenuItems = mainMenuPage.getAllMenuItems();
+
+        // Assert if actual and expected menu items are the same
+        Assert.assertEquals(actualMenuItems, expectedMenuItems,
+                "Menu items do not match the expected list");
+    }
 
 	@AfterMethod
 	public void tearDown() {
